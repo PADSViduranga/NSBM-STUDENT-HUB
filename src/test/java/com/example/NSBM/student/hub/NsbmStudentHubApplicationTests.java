80b4e769a3879f0907c88f70eda4de3348728e15
sachinthavidu <padsviduranga@students.nsbm.ac.lk>
2026-02-09 15:23:47 +0530

    first

---
+++ src/test/java/com/example/NSBM/student/hub/NsbmStudentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NSBM.student.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsbmStudentHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
